bf00e0e1291433ce4cfbf8cadbf9caf18b73dc59
DemetrioQ <demetriorqe@gmail.com>
2021-05-21 18:36:45 -0400

    Testing creating new routes

---
+++ src/components/ScoresComponent.vue
@@ -0,0 +1,16 @@
+<template>
+  <div class="scores">
+    <h1>{{msg}}</h1>
+  </div>
+</template>
+
+<script lang="ts">
+import Vue from "vue";
+
+export default Vue.extend({
+  name: "ScoresComponent",
+  props: {
+    msg: String,
+  },
+});
+</script>
---
+++ src/views/Scores.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="scores">
+    <ScoresComponent msg="Scores" />
+  </div>
+</template>
+
+<script lang="ts">
+import Vue from "vue";
+import ScoresComponent from "@/components/ScoresComponent.vue"; // @ is an alias to /src
+
+export default Vue.extend({
+  name: "Scores",
+  components: {
+    ScoresComponent,
+  },
+});
+</script>
